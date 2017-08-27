bf706b68e6f590d67323d91f911e5edf4e1fa09a
solominh <hoangminhftu2@gmail.com>
2017-08-28 01:37:09 +0700

    - Use Nodejs eventEmitter.listenerCount  => increase checking performance - Add options parameters to emitter.startWatching({watchDelay}) - Remove electron and events module from package.json

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
-const {clipboard} = require('electron')
-const EventEmitter = require('./EventEmitter')
-const clipboardEmitter = new EventEmitter()
+const { clipboard } = require("electron");
+const EventEmitter = require("events");
+const clipboardEmitter = new EventEmitter();
 
-let watcherId = null, previousText = clipboard.readText(), previousImage = clipboard.readImage()
+export const TEXT_CHANGED_EVENT = "text-changed";
+export const IMAGE_CHANGED_EVENT = "image-changed";
+
+let watcherId = null;
+let previousText = clipboard.readText();
+let previousImage = clipboard.readImage();
 
 clipboard.on = (event, listener) => {
-    clipboardEmitter.on(event, listener)
-    return clipboard
-}
+  clipboardEmitter.on(event, listener);
+  return clipboard;
+};
 
 clipboard.once = (event, listener) => {
-    clipboardEmitter.once(event, listener)
-    return clipboard
-}
+  clipboardEmitter.once(event, listener);
+  return clipboard;
+};
 
 clipboard.off = (event, listener) => {
-    if(listener)
-        clipboardEmitter.removeListener(event, listener)
-    else
-        clipboardEmitter.removeAllListeners(event)
-    return clipboard
-}
+  if (listener) clipboardEmitter.removeListener(event, listener);
+  else clipboardEmitter.removeAllListeners(event);
+  return clipboard;
+};
 
-clipboard.startWatching = () => {
-    if(!watcherId)
-        watcherId = setInterval(() => {
-            if(isDiffText(previousText, previousText = clipboard.readText())) clipboardEmitter.emit('text-changed')
-            if(isDiffImage(previousImage, previousImage = clipboard.readImage())) clipboardEmitter.emit('image-changed')
-        }, 500)
-    return clipboard
-}
+clipboard.startWatching = ({ watchDelay } = { watchDelay: 500 }) => {
+  if (!watcherId)
+    watcherId = setInterval(() => {
+      if (
+        clipboardEmitter.listenerCount(TEXT_CHANGED_EVENT) > 0 &&
+        isDiffText(previousText, (previousText = clipboard.readText()))
+      ) {
+        clipboardEmitter.emit(TEXT_CHANGED_EVENT);
+      }
+
+      if (
+        clipboardEmitter.listenerCount(IMAGE_CHANGED_EVENT) > 0 &&
+        isDiffImage(previousImage, (previousImage = clipboard.readImage()))
+      ) {
+        clipboardEmitter.emit(IMAGE_CHANGED_EVENT);
+      }
+    }, watchDelay);
+
+  return clipboard;
+};
 
 clipboard.stopWatching = () => {
-    if(watcherId) clearInterval(watcherId)
-    watcherId = null
-    return clipboard
+  if (watcherId) clearInterval(watcherId);
+  watcherId = null;
+  return clipboard;
+};
+
+function isDiffText(str1, str2) {
+  return str2 && str1 !== str2;
 }
 
-function isDiffText(str1, str2){
-    return str2 && str1 !== str2
+function isDiffImage(img1, img2) {
+  return !img2.isEmpty() && img1.toDataURL() !== img2.toDataURL();
 }
 
-function isDiffImage(img1, img2){
-    return !img2.isEmpty() && img1.toDataURL() !== img2.toDataURL()
-}
-
-module.exports = clipboard
+module.exports = clipboard;
